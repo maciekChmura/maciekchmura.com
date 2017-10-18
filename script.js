aaf2f29b768db93e8bbbaed731576876904df121
Maciek Chmura <chmura.m@gmail.com>
2017-10-18 11:35:53 +0200

    cv and new project

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ var typed = new Typed(".typed2",
   var typed = new Typed(".typed4",
   {
     strings: [
-      "React, PWA, charts and lots more",
+      "React, Redux, and more...",
       ],
     startDelay: 12000,
     typeSpeed: 80,
@@ -47,36 +47,4 @@ var typed = new Typed(".typed2",
     loop: false
   });
 
-
-
-
-
-
-
-//Bulma control
-  document.addEventListener('DOMContentLoaded', function () {
-    
-      // Get all "navbar-burger" elements
-      var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
-    
-      // Check if there are any navbar burgers
-      if ($navbarBurgers.length > 0) {
-    
-        // Add a click event on each of them
-        $navbarBurgers.forEach(function ($el) {
-          $el.addEventListener('click', function () {
-    
-            // Get the target from the "data-target" attribute
-            var target = $el.dataset.target;
-            var $target = document.getElementById(target);
-    
-            // Toggle the class on both the "navbar-burger" and the "navbar-menu"
-            $el.classList.toggle('is-active');
-            $target.classList.toggle('is-active');
-    
-          });
-        });
-      }
-    
-    });
   
